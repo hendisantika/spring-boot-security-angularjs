d8503c5376be72e0be5ba5b2392bddd34d1b154a
Hendi Santika <hendisantika@gmail.com>
2018-07-29 05:43:13 +0700

    #1 - First Commit

---
+++ src/main/java/com/hendisantika/springbootsecurityangularjs/SpringBootSecurityAngularjsApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootsecurityangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityAngularjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityAngularjsApplication.class, args);
+    }
+}
